fa619065e3fda99502cb97b4f662216543da5466
Ivan Pacheco <ivanpachecoleo23@gmail.com>
2025-06-30 10:15:57 -0500

    init commit

--- tasks/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import View
-from rest_framework import viewsets
-from .serializer import TaskSerializer
-from .models import Tasks
-
-
-class TaskView(viewsets.ModelViewSet):
-    serializer_class = TaskSerializer
-    queryset = Tasks.objects.all()
