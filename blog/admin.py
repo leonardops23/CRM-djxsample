674566abf1eeeed6ede71f8eaf66f768c5020f78
Ivan Pacheco <ivanpachecoleo23@gmail.com>
2025-07-31 17:53:20 -0500

    crm initv1

--- blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content')
-    list_filter = ('title', 'content')
-    
-
-admin.site.register(Post, PostAdmin)
